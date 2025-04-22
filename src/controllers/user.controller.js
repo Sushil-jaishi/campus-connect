5a1edb2953b0a737933e9a16ad5d02f800c26f0e
Sushil Jaishi <sushiljaishi@gmail.com>
2025-04-22 08:39:16 +0545

    refresh access token feature implemented

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
+import jwt from "jsonwebtoken"
 
 const cookieOptions = {
   httpOnly: true,
@@ -118,4 +119,43 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "user logged out"))
 })
 
-export { registerUser, loginUser, logoutUser }
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  try {
+    const incommingRefreshToken =
+      req.cookies?.refreshToken || req.body.refreshToken
+
+    if (!incommingRefreshToken) {
+      throw new ApiError(401, "Refresh token missing")
+    }
+
+    const decodedToken = jwt.verify(
+      incommingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRETE
+    )
+    const user = await User.findById(decodedToken._id)
+
+    if (!user || incommingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(403, "invalid refresh token")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+      user._id
+    )
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, cookieOptions)
+      .cookie("refreshToken", refreshToken, cookieOptions)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken },
+          "Access Token Refreshed"
+        )
+      )
+  } catch (error) {
+    throw new ApiError(403, "Token invalid or expired")
+  }
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken }
